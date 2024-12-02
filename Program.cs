cac5cd0508bebe28d0910c526cd84b44cb3bb12d
Evgeni <apostle101@yandex.ru>
2024-12-02 17:19:40 +0300

    изменил конструктор

--- Program.cs
+++ Program.cs
@@ -4,24 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using OfficeOpenXml;
+
 using System.IO;
 using Newtonsoft.Json.Linq;
 using System.Security.Cryptography;
 
 using Xceed.Words.NET;
+using System.Net;
+using Xceed.Document.NET;
 
 namespace ConsoleApp1
 {
-    
+
     internal class Program
     {
-        
 
         static JObject GetBankDataByBik(string bik)
         {
-            string jsonFilePath = @"C:\Users\Евгений\Desktop\1.json";
-            var jsonData = File.ReadAllText(jsonFilePath);
+            //string jsonFilePath = @"C:\Users\Евгений\Desktop\1.json";
+            var jsonData = File.ReadAllText("1.json");
             var banks = JArray.Parse(jsonData);
 
             var bank = banks.FirstOrDefault(b => b["bik"].ToString() == bik);
@@ -31,178 +32,52 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            WriteLine("Введите наименование контрагента:");
-            string clientName = ReadLine();
-            WriteLine("Введите имя клиента:");
-            string clientFIO = ReadLine();
-            WriteLine("Введите ИНН клиента:");
-            string clientINN = ReadLine();
-            WriteLine("Введите ОГРН клиента:");
-            string clientOGRN = ReadLine();
-            WriteLine("Введите расчётный счёт клиента:");
-            string CurrentAccount = ReadLine();
+            Console.WriteLine("Введите наименование контрагента:");
+            string counterparty = Console.ReadLine();
+            Console.WriteLine("Введите имя клиента:");
+            string director = Console.ReadLine();
+            Console.WriteLine("Введите ИНН клиента:");
+            string inn = Console.ReadLine();
+            Console.WriteLine("Введите ОГРН клиента:");
+            string ogrn = Console.ReadLine();
+            Console.WriteLine("Введите расчётный счёт клиента:");
+            string currentaccount = Console.ReadLine();
 
-            WriteLine("Введите БИК банка:");
-            string bik = ReadLine();
-            string ks = String.Empty;
-            string name = String.Empty;
+            Console.WriteLine("Введите БИК банка:");
+            string bik = Console.ReadLine();
+            string correspondentaccount = String.Empty;
+            string bankname = String.Empty;
 
             // Загрузка данных из JSON файла
             var bankData = GetBankDataByBik(bik);
 
             if (bankData != null)
             {
-                ks = bankData["ks"].ToString();
-                name = bankData["name"].ToString();
+                correspondentaccount = bankData["ks"].ToString();
+                bankname = bankData["name"].ToString();
                 //{bankData["address"]};
             }
             else
             {
-                WriteLine("Информация о банке не найдена.");
+                Console.WriteLine("Информация о банке не найдена.");
             }
 
-
-            WriteLine("Введите адрес клиента:");
-            string ClientAdres = ReadLine();
-
-            WriteLine("введите наименование товара");
-            string product = ReadLine();    
-            WriteLine("введите количество");
-            int amount = Convert.ToInt32(ReadLine());
-            WriteLine("введите цену за еденицу");
-            double price = Convert.ToDouble(ReadLine());
-            double result = price * amount;
-
-            DocumentGenerator check = new DocumentGenerator();
-            check.CreateContract(
-                check.now, clientName, clientINN, ClientAdres, CurrentAccount, bik, ks, name, @"D:\document\счёт шаблон.docx",
-                product, amount, price, result);
-
-            DocumentGenerator productsList = new DocumentGenerator();
-            productsList.CreateProductsList(
-                productsList.now, clientName, clientINN, ClientAdres, CurrentAccount, bik, ks, name, @"D:\document\Накладная_ТОРГ-12.docx",
-product, amount, price, result);
-
-            DocumentGenerator docGen = new DocumentGenerator();
-            docGen.CreateContract(
-                docGen.now, clientName, clientINN, clientOGRN, ClientAdres, CurrentAccount, bik, ks, name, clientFIO, @"D:\document\Договор Поставки.docx");
-
-            Read();
-        }
-/*
-        public class DocumentGenerator
-        {
-            public void CreateContract(
-                DateTime contractDate, string clientName, string clientINN, string clientAdres, string CurrentAccount, string bik, string ks, string name, string outputPath)
-            {
-                string checkPath = "счёт шаблон.docx";
-                using (DocX document = DocX.Load(checkPath))
-                {
-                    // Замена заполнителей на реальные данные
-                    //ввод номера договора
-                    document.ReplaceText("{{ContractNumber}}", now.ToString("ddMMyyyy"));
-                    //ввод даты договора
-                    document.ReplaceText("{{ContractDate}}", now.ToString("dd.MM.yyyy"));
-                    //ввод наименование контрагента
-                    document.ReplaceText("{{ClientName}}", clientName);
-                    //ввод ИНН контрагента
-                    document.ReplaceText("{{clientINN}}", clientINN);
-                    //ввод ОГРН
-                    //document.ReplaceText("{{clientOGRN}}", clientOGRN);
-                    //ввод адреса
-                    document.ReplaceText("{{ClientAdres}}", clientAdres);
-                    //ввод расчётного счёта
-                    document.ReplaceText("{{CurrentAccount}}", CurrentAccount);
-                    //ввод БИК банка
-                    document.ReplaceText("{{bik}}", bik);
-                    // ввод кор-счёта
-                    document.ReplaceText("{{ks}}", ks);
-                    //имя банка
-                    document.ReplaceText("{{name}}", name);
-                    //ФИО уполномоченного лица
-                    //document.ReplaceText("{{clientFIO}}", clientFIO);
+            Console.WriteLine("Введите адрес клиента:");
+            string address = Console.ReadLine();
+            Console.WriteLine("введите наименование товара");
+            string product = Console.ReadLine();
+            Console.WriteLine("введите количество");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("введите цену за еденицу");
+            decimal price = Convert.ToDecimal(Console.ReadLine());
+            decimal result = price * amount;
 
 
-                    // Сохранение заполненного документа
-                    document.SaveAs(outputPath);
-                }
-            }
-
-            public void CreateContract(
-                DateTime contractDate, string clientName, string clientINN, string clientOGRN, string clientAdres, string CurrentAccount, string bik, string ks, string name, string clientFIO, string outputPath)
-            {
-                string templatePath = "Договор Поставки.docx";
-
-                using (DocX document = DocX.Load(templatePath))
-                {
-                    // Замена заполнителей на реальные данные
-                    //ввод номера договора
-                    document.ReplaceText("{{ContractNumber}}", now.ToString("ddMMyyyy"));
-                    //ввод даты договора
-                    document.ReplaceText("{{ContractDate}}", now.ToString("dd.MM.yyyy"));
-                    //ввод наименование контрагента
-                    document.ReplaceText("{{ClientName}}", clientName);
-                    //ввод ИНН контрагента
-                    document.ReplaceText("{{clientINN}}", clientINN);
-                    //ввод ОГРН
-                    document.ReplaceText("{{clientOGRN}}", clientOGRN);
-                    //ввод адреса
-                    document.ReplaceText("{{ClientAdres}}", clientAdres);
-                    //ввод расчётного счёта
-                    document.ReplaceText("{{CurrentAccount}}", CurrentAccount);
-                    //ввод БИК банка
-                    document.ReplaceText("{{bik}}", bik);
-                    // ввод кор-счёта
-                    document.ReplaceText("{{ks}}", ks);
-                    //имя банка
-                    document.ReplaceText("{{name}}", name);
-                    //ФИО уполномоченного лица
-                    document.ReplaceText("{{clientFIO}}", clientFIO);
-
-
-                    // Сохранение заполненного документа
-                    document.SaveAs(outputPath);
-                }
-            }
+            Replacedata document = new Replacedata(
+                counterparty, director, inn, ogrn, address, currentaccount, bik, correspondentaccount, bankname, product, amount, price, result);
             
-            public void CreateProductsList(
-               DateTime contractDate, string clientName, string clientINN, string clientAdres, string CurrentAccount, string bik, string ks, string name, string outputPath)
-            {
-                string goodslistPath = "Накладная_ТОРГ-12.docx";
-                using (DocX document = DocX.Load(goodslistPath))
-                {
-                    // Замена заполнителей на реальные данные
-                    //ввод номера договора
-                    document.ReplaceText("{{ContractNumber}}", now.ToString("ddMMyyyy"));
-                    //ввод даты договора
-                    document.ReplaceText("{{ContractDate}}", now.ToString("dd.MM.yyyy"));
-                    //ввод наименование контрагента
-                    document.ReplaceText("{{ClientName}}", clientName);
-                    //ввод ИНН контрагента
-                    document.ReplaceText("{{clientINN}}", clientINN);
-                    //ввод ОГРН
-                    //document.ReplaceText("{{clientOGRN}}", clientOGRN);
-                    //ввод адреса
-                    document.ReplaceText("{{ClientAdres}}", clientAdres);
-                    //ввод расчётного счёта
-                    document.ReplaceText("{{CurrentAccount}}", CurrentAccount);
-                    //ввод БИК банка
-                    document.ReplaceText("{{bik}}", bik);
-                    // ввод кор-счёта
-                    document.ReplaceText("{{ks}}", ks);
-                    //имя банка
-                    document.ReplaceText("{{name}}", name);
-                    //ФИО уполномоченного лица
-                    //document.ReplaceText("{{clientFIO}}", clientFIO);
-
-
-                    // Сохранение заполненного документа
-                    document.SaveAs(outputPath);
-                }
-            }
-            
+            Console.WriteLine("готов");
+            Console.Read();
         }
-
-*/
     }
 }
---
+++ Replacedata.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xceed.Words.NET;
+
+namespace ConsoleApp1
+{
+    internal class Replacedata
+    {
+        public DateTime now = DateTime.Now;
+        List<string> templatePath = new List<string>
+        { "Договор Поставки.docx", "Накладная_ТОРГ-12.docx", "счёт шаблон.docx"};
+
+        string outputDirectory = "D:\\document\\";
+
+        public Replacedata(
+            string counterparty, string director, string inn, string ogrn, string address, string currentaccount, string bik, string correspondentaccount, string bankname, String product, int amount, decimal price, decimal result)
+        {
+            foreach (string tamplepath in templatePath)
+            {
+                string outputPath = Path.Combine(outputDirectory, Path.GetFileName(tamplepath));
+
+                using (DocX document = DocX.Load(tamplepath))
+                {
+                    // Замена заполнителей на реальные данные
+                    //ввод номера договора
+                    document.ReplaceText("{{ContractNumber}}", now.ToString("ddMMyyyy"));
+                    //ввод даты договора
+                    document.ReplaceText("{{ContractDate}}", now.ToString("dd.MM.yyyy"));
+                    //ввод наименование контрагента
+                    document.ReplaceText("{{ClientName}}", counterparty);
+                    //ввод ИНН контрагента
+                    document.ReplaceText("{{clientINN}}", inn);
+                    //ввод ОГРН
+                    document.ReplaceText("{{clientOGRN}}", ogrn);
+                    //ввод адреса
+                    document.ReplaceText("{{ClientAdres}}", address);
+                    //ввод расчётного счёта
+                    document.ReplaceText("{{CurrentAccount}}", currentaccount);
+                    //ввод БИК банка
+                    document.ReplaceText("{{bik}}", bik);
+                    // ввод кор-счёта
+                    document.ReplaceText("{{ks}}", correspondentaccount);
+                    //имя банка
+                    document.ReplaceText("{{name}}", bankname);
+                    //ФИО уполномоченного лица
+                    document.ReplaceText("{{clientFIO}}", director);
+
+                    //товар
+                    document.ReplaceText("{{product}}", product);
+                    //количество
+                    document.ReplaceText("{{amount}}", amount.ToString());
+                    //цена
+                    document.ReplaceText("{{price}}", price.ToString());
+                    //сумма
+                    document.ReplaceText("{{result}}", result.ToString());
+
+                    // Сохранение заполненного документа
+                    document.SaveAs(outputPath);
+                }
+            }
+
+        }
+    }
+}
